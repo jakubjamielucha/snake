928b5f7c43fccf14e63e9aef4fb2c3cb1c74f2dc
jakubjamielucha <jakubjamielucha@gmail.com>
2023-12-05 02:33:05 +0100

    Developing the logic of Snake length and adding the Game Over sign at the end of the game.

--- src/Board.java
+++ src/Board.java
@@ -1,12 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Board extends JPanel {
+public class Board extends JPanel implements ActionListener{
 
     private Image apple, mint1, mint2;
-    private int dots;
+    private int dots, appleX, appleY;
     private final int DOT_SIZE = 10, ALL_DOTS = 900, RANDOM_POSITION = 29;
     private final int x[] = new int[ALL_DOTS], y[] = new int [ALL_DOTS];
+    private Timer timer;
+    private boolean inGame = true;
 
     Board(){
         setBackground(Color.BLACK);
@@ -31,7 +34,57 @@ public class Board extends JPanel {
         for(int z = 0; z<dots; z++){
             x[z] = 50 - z * DOT_SIZE;
             y[z] = 50;
+            locateApple();
+            timer = new Timer(140, this);
+            timer.start();
         }
     }
 
+    public void locateApple(){
+        int r = (int)(Math.random()*RANDOM_POSITION);
+        appleX = (r*DOT_SIZE);
+        r = (int)(Math.random()*RANDOM_POSITION);
+        appleY = (r*DOT_SIZE);
+    }
+
+    public void checkApple(){
+        if(x[0] == appleX && y[0] == appleY){
+            dots++;
+            locateApple();
+        }
+    }
+
+    public void draw (Graphics g){
+        if(inGame){
+            g.drawImage(apple, appleX, appleY, this);
+
+            for(int z = 0; z<dots; z++){
+            if(z==0){
+                g.drawImage(mint1, x[z], y[z], this);
+            } else {
+                g.drawImage(mint2, x[z], y[z], this);
+            }
+        }
+        Toolkit.getDefaultToolkit().sync();
+    } else {
+        gameOver(g);
+    }
+    }
+
+    public void gameOver(Graphics g){
+        String msg = "GAME OVER";
+        Font font = new Font("SAN_SERIF", Font.BOLD, 16);
+        FontMetrics metrices = getFontMetrics(font);
+
+        g.setColor(Color.WHITE);
+        g.setFont(font);
+        g.drawString(msg, (300 - metrices.stringWidth(msg))/2, 300/2);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if(inGame){
+            checkApple();
+        }
+    }
 }
